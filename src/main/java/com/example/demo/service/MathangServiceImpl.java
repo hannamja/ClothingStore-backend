d662a7e31772a36de558c75b124ea7b5feeb6d89
kietdz <thelight3501@gmail.com>
2023-10-19 14:20:15 +0700

    commit

--- src/main/java/com/example/demo/service/MathangServiceImpl.java
+++ src/main/java/com/example/demo/service/MathangServiceImpl.java
@@ -85,7 +85,7 @@ public class MathangServiceImpl implements MathangService {
 		for (Mathang mh : list) {
 			List<CtKhuyenmai> temp = ctKhuyenmaiRepository.getKhuyenmai(mh.getMamh());
 			Gia gia = giaRepository.getBanggiaLast(mh.getMamh());
-			if (mh.getTrangthai().equals("0") && gia !=null)
+			if (mh.getTrangthai().equals("0") && gia !=null && chitietMathangRepository.getCTMathang(mh.getMamh()).size() != 0)
 				if (temp.size() == 0)
 					mathangDTOs.add(mathangConvert.toDTO(mh, gia,
 							hinhanhRepository.getHAByMH(mh.getMamh()),
