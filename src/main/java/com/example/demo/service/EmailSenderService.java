d4aec05bc3cce349d65815ed3cb6edf62bdab5d0
Phuong Nguyen <phuongsover2@gmail.com>
2023-10-24 15:39:23 +0700

    thêm quên mật khẩu

---
+++ src/main/java/com/example/demo/service/EmailSenderService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class EmailSenderService {
+  private final JavaMailSender mailSender;
+  
+  public void sendEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("phuongsover2@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+    }
+  
+}
